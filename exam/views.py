27a2cd330a4fea13dfcec50478ecbfee5754fecf
Deepesh Genani <72039675+ASCE-D@users.noreply.github.com>
2022-03-09 13:01:26 +0530

    Update views.py

--- exam/views.py
+++ exam/views.py
@@ -1,14 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def teacher(request):
+    if request.user.is_anonymous:
+        return render(request, 'teacher_signin.html')
     return render(request, 'teacher.html')
 
 def student(request):
-    return render(request, 'student.html')
+    if request.user.is_anonymous:
+        return render(request, "student_signin.html")
+    else:
+        return render(request, 'student.html')
 
 def sets(request):
     return render(request, 'set.html')
@@ -18,3 +25,38 @@ def check(request):
 
 def make(request):
     print("Hiiiiiiii")
+
+
+# ================== creating a student login function ======================
+
+def login_student(request):
+    if request.method == 'POST':                   # checking if the method is POST
+        user = request.POST.get('username')        # storing the value of username in username variable
+        passw = request.POST.get('password')       # storing value of the password in the password variable
+        
+    # ============ authentication =================
+        user = authenticate(username = user, password = passw)           # creating instance for aunthentication from models 
+        if user is not None :                                            # checking if user is not none 
+            login(request, user)                                         # calling login function by passing request and the instance of authentication
+            return render(request, "student.html")                       # redirecting to the student webpage on succesful authentication
+        else:                                                            
+            return render(request, "index.html")                         #returning to homepage 
+
+    return render(request, "index.html")
+
+
+# ==================== creating Teacher login function =========================
+def login_teacher(request):
+    if request.method == 'POST':
+        user = request.POST.get('username')
+        passw = request.POST.get('password')
+
+        # ======== authentication ===========
+
+        user = authenticate(username = user, password = passw)
+        if user is not None:
+
+            return render(request, "teacher.html")
+        else:
+            return render(request, "index.html")
+
