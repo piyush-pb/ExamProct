a7ef4a6b98a64eac55df22c21a4c749a5251b115
Piyush-onGIT <thisissahupiyush@gmail.com>
2022-03-10 10:28:48 +0530

    bug fixes

--- Exam/urls.py
+++ Exam/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from exam import views as ev
 from django.views.static import serve
-# from django.conf.urls import url
-from django.urls import re_path as url
+# from django.urls import re_path as url
+from django.conf.urls import url
 from django.conf import settings
 
 urlpatterns = [
